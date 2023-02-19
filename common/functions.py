e8472bb52dc74d272713893889a428fe4525cdfa
ekuroda <kurodaeishi@gmail.com>
2023-02-19 17:12:59 +0900

    ch06

--- common/functions.py
+++ common/functions.py
@@ -28,12 +28,12 @@ def cross_entropy_error(y, t):
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
 
-    # 教示データがone-hot-vectorなら正解ラベルのインデックスに変換
+    # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
     if t.size == y.size:
         t = t.argmax(axis=1)
 
     batch_size = y.shape[0]
-    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7))
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
 
 def softmax_loss(X, t):
     y = softmax(X)
---
+++ common/util.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+
+def smooth_curve(x):
+    window_len = 11
+    s = np.r_[x[window_len-1:0:-1], x, x[-1:-window_len:-1]]
+    w = np.kaiser(window_len, 2)
+    y = np.convolve(w/w.sum(), s, mode='valid')
+    return y[5:len(y)-5]
+
+
+def shuffle_dataset(x, t):
+    """データセットのシャッフルを行う
+    Parameters
+    ----------
+    x : 訓練データ
+    t : 教師データ
+    Returns
+    -------
+    x, t : シャッフルを行った訓練データと教師データ
+    """
+    permutation = np.random.permutation(x.shape[0])
+    x = x[permutation,:] if x.ndim == 2 else x[permutation,:,:,:]
+    t = t[permutation]
+
+    return x, t
